d3d0fa10b4806e85720e87abad5558b336f5cb5f
Unknown <maria.kisenga@strathmore.edu>
2018-07-04 14:41:34 +0300

    b

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register','UserController@register');
+Route::post('/login','UserController@login');
+Route::get('/showusers', 'UserController@showUsers');
+
+//Session 
+Route::post('/save_session','SessionController@save');
+Route::get('/show_session','SessionController@session');
+
+//Instructors
+Route::post('/saveInstructor','InstructorController@saveInstructor');
+Route::get('/showinstructors','InstructorController@showinstructors');
+
+//Gym
+Route::post('/saveGym','LocationController@saveGym');
+Route::get('/showGym','LocationController@showGym');
+
+
