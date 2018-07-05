f4b9ec0b3069487974cb91c9ede88cfb6ce156da
Unknown <maria.kisenga@strathmore.edu>
2018-07-05 10:22:54 +0300

    hgfs

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('register','UserController@addUser');
-Route::post('login','UserController@loginUser');
+Route::post('/register','UserController@addUser');
+Route::post('/login','UserController@loginUser');
 Route::get('/showusers', 'UserController@showUsers');
 
-Route::post('save_session','SessionController@save');
+Route::post('/save_session','SessionController@save');
 Route::get('/show_sessions','SessionController@getAll');
 
 Route::get('/showinstructors','InstructorController@getAll');
 
-Route::post('saveGym','LocationController@saveGym');
+Route::post('/saveGym','LocationController@saveGym');
 Route::get('/showGym','LocationController@showGym');
 
 
