8a48d1d93ca36d33895760cb763f2ee327c6961f
Unknown <maria.kisenga@strathmore.edu>
2018-07-05 04:42:16 +0300

    user3

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,19 @@ Route::get('/', function () {
 });
 
 //User
-Route::post('/register','UserController@addUser');
-Route::post('/login','UserController@login');
+Route::post('register','UserController@addUser');
+Route::post('login','UserController@login');
 Route::get('/showusers', 'UserController@showUsers');
 
 //Session 
-Route::post('/save_session','SessionController@save');
+Route::post('save_session','SessionController@save');
 Route::get('/show_session','SessionController@session');
 
 //Instructors
 Route::get('/showinstructors','InstructorController@getAll');
 
 //Gym
-Route::post('/saveGym','LocationController@saveGym');
+Route::post('saveGym','LocationController@saveGym');
 Route::get('/showGym','LocationController@showGym');
 
 
