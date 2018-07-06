3680788bd6fd017eb553cc651f82164adc14fc6a
Unknown <maria.kisenga@strathmore.edu>
2018-07-06 08:37:20 +0300

    sess3

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -8,32 +8,16 @@ use App\locations_94910;
 
 class LocationController extends Controller
 {
-    public function saveGym(Request $req)
-	{
-		$gym_name = $req['gym_name'];
-		$location = $req['location'];
-		$opening_time = $req['opening_time'];
-		$closing_time = $req['closing_time'];
+    
 
-		$locations_94910 = new locations_94910;
-		$locations_94910->gym_name = $gym_name;
-		$locations_94910->location = $location;
-		$locations_94910->opening_time = $opening_time;
-		$locations_94910->closing_time = $closing_time;
-
-		$locations_94910->save();
-
-		return $locations_94910->toJson();
-	}
-
-	public function showGym(Request $req)
+	public function showGymLocation(Request $req)
 	{
 		$data = locations_94910::all();
 
 		return $data->toJson();
 	}
 
-	public function gymNear(Request $req)
+	public function nearGymLocation(Request $req)
 	{
 		$location->req->input('location');
 
--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,14 @@ Route::get('/', function () {
 
 Route::post('/register','UserController@addUser');
 Route::post('/login','UserController@loginUser');
-//Route::get('/showusers', 'UserController@showUsers');
 
 Route::post('/add_session','SessionController@addSession');
 Route::get('/show_sessions','SessionController@getAll');
 
 Route::get('/showinstructors','InstructorController@getAll');
 
-Route::post('/saveGym','LocationController@saveGym');
-Route::get('/showGym','LocationController@showGym');
+Route::get('/showGym','LocationController@showGymLocation');
+Route::get('/nearGym','LocationController@nearGymLocation');
 
 
 ?>
