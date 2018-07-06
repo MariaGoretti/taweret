8751a1d4661c1b3774738c563581bb0a0cfa8e5c
Unknown <maria.kisenga@strathmore.edu>
2018-07-06 03:58:03 +0300

    sess2

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -22,43 +22,39 @@ if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+$query = "SELECT session_id, exercise, sets, location, session_date FROM sessions_94910";
+$result = array();
 $sessionArray = array();
 $response = array();
-
-    $query = "SELECT session_id, exercise, sets, location, session_date FROM sessions_94910 WHERE user_id=?";
-    if($stmt = $con->prepare($query)){
-        $stmt->bind_param("i",$user_id);
-        $stmt->execute();
-        //Bind fetched result to $ variables
-        $stmt->bind_result($session_id, $exercise, $sets, $location, $session_date);
-        //Check for results     
-        if($stmt){
-            while($row = $stmt->fetch()) {
-            $sessionArray["session_id"] = $session_id;
-            $sessionArray["exercise"] = $exercise;
-            $sessionArray["sets"] = $sets;
-            $sessionArray["location"] = $location;
-            $sessionArray["session_date"] = $session_date;
-             }
-            $response["success"] = 1;
-            $response["data"] = $sessionArray;
+//Prepare the query
+if($stmt = $con->prepare($query)){
+    $stmt->execute();
+    
+    $stmt->bind_result($session_id,$exercise, $sets, $location, $session_date);
+    //Fetch 1 row at a time                 
+    while($stmt->fetch()){
         
+        $sessionArray["session_id"] = $session_id;
+        $sessionArray["exercise"] = $exercise;
+        $sessionArray["sets"] = $sets;
+        $sessionArray["location"] = $location;
+        $sessionArray["session_date"] = $session_date;
         
-        }else{
-            
-            $response["success"] = 0;
-            $response["message"] = "No sessions yet";
-        }
-        $stmt->close();
-
-
-    }else{
-        //When some error occurs
-        $response["success"] = 0;
-        $response["message"] = mysqli_error($con);
+        $result[]=$sessionArray;
         
     }
-
+    $stmt->close();
+    $response["success"] = 1;
+    $response["data"] = $result;
+    
+ 
+}else{
+    //Some error while fetching data
+    $response["success"] = 0;
+    $response["message"] = mysqli_error($con);
+        
+    
+}
 //Display JSON response
 echo json_encode($response);
   }
